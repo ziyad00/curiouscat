e30be9082cdadedd7d6caef9eb10933bd98bd037
ziyad00 <ziyad.alotaibe@gmail.com>
2020-09-19 04:11:26 +0300

    s

--- sarahqa/settings.py
+++ sarahqa/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'home'
@@ -181,11 +181,3 @@ REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_DB = 0
 django_heroku.settings(locals())
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
